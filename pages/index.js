4bdbeaf60632c3231af925902ce62819a426a8b2
Mayon Francis <mayonfrancis@gmail.com>
2021-12-19 17:40:36 +0530

    next starter template

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Editing from local machine and testing if auto deploy works! <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
