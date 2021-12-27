462001d44a9e9824349fce372348af8a5616a88b
Mayon Francis <mayonfrancis@gmail.com>
2021-12-27 23:21:07 +0530

    Added support for netlify environment apparently last one didn't work, trying removing quotes around the key in object blah blah

--- utils/db/index.js
+++ utils/db/index.js
@@ -1,22 +1,25 @@
 const admin = require('firebase-admin');
+var serviceAccount;
 try{
     // For Dev Build on local Machine
-    const serviceAccount = require('./serviceAccountKey.json');
+    serviceAccount = require('./serviceAccountKey.json');
+    console.log(serviceAccount)
 }
 catch{
     // For Netlify Deployment
-    const serviceAccount = {
-      "type": process.env.type,
-      "project_id": process.env.project_id,
-      "private_key_id": process.env.private_key_id,
-      "private_key": process.env.private_key,
-      "client_email": process.env.client_email,
-      "client_id": process.env.client_id,
-      "auth_uri": process.env.auth_uri,
-      "token_uri": process.env.token_uri,
-      "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-      "client_x509_cert_url": process.env.client_x509_cert_url
+    serviceAccount = {
+      type: process.env.type,
+      project_id: process.env.project_id,
+      private_key_id: process.env.private_key_id,
+      private_key: process.env.private_key,
+      client_email: process.env.client_email,
+      client_id: process.env.client_id,
+      auth_uri: process.env.auth_uri,
+      token_uri: process.env.token_uri,
+      auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+      client_x509_cert_url: process.env.client_x509_cert_url
     }
+    console.log(serviceAccount)
 }
 //initialize admin SDK using serviceAcountKey
 if (!admin.apps.length) {
@@ -27,5 +30,5 @@ if (!admin.apps.length) {
     } catch (error) {
       console.log('Firebase admin initialization error', error.stack);
     }
-  }
+}
   export default admin.firestore();
