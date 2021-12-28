dd8f42b6a0806ce6affdb857da521ae56459265f
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2021-12-28 17:10:26 +0530

    added rudimentary feed page

---
+++ pages/feed/index.js
@@ -0,0 +1,32 @@
+import {useRef, useState} from "react";
+import axios from 'axios';
+
+
+
+export default function feed(){
+    const [posts, setPosts] =  useState([]);
+    async function getPosts(){
+        try{
+         const res = await axios.get('/api/getBlogs');
+         console.log(res.data);
+          setPosts(res.data);
+           }catch(err){
+               console.log(err);
+               }
+       }
+ getPosts();
+ console.log(posts);
+return(
+    <div>
+    {posts.map(post => (
+        <div>
+            <h1>{post.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: post.content}} />
+          </div>   
+    ))}
+    </div>
+)
+
+}
+
+
