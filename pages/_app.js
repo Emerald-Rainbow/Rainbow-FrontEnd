69f6fb946ac3a6d198cdf5fe7bd07b445d87d502
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2021-12-29 18:33:19 +0530

    styled feed page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '@styles/globals.css'
-
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import Head from 'next/head'
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ 
+  return( 
+    <div>
+    <Head>
+    <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+   </Head>
+  <Component {...pageProps} />
+  </div>
+  )
 }
 
 export default Application
