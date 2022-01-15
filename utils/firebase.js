bed2286263a6578c74ab202294627466c43bbc60
Mayon-Francis <75270610+Mayon-Francis@users.noreply.github.com>
2022-01-15 22:12:39 +0530

    Testing Api Key Access

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,16 +3,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from  "firebase/auth"
 import { getFirestore } from "firebase/firestore"
+import firebaseConfig from './db/firebaseConfig';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDUS0f4hgMymr9boSYiuJQL9UKwkIVVU_o",
-  authDomain: "rainbow-temp.firebaseapp.com",
-  projectId: "rainbow-temp",
-  storageBucket: "rainbow-temp.appspot.com",
-  messagingSenderId: "911580734101",
-  appId: "1:911580734101:web:d81f7a62f256df24c8319c",
-  measurementId: "G-GDT2XS4VZS"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDUS0f4hgMymr9boSYiuJQL9UKwkIVVU_o",
+//   authDomain: "rainbow-temp.firebaseapp.com",
+//   projectId: "rainbow-temp",
+//   storageBucket: "rainbow-temp.appspot.com",
+//   messagingSenderId: "911580734101",
+//   appId: "1:911580734101:web:d81f7a62f256df24c8319c",
+//   measurementId: "G-GDT2XS4VZS"
+// };
 
   const app = initializeApp(firebaseConfig);
 
