43829fbf78521ee9e4788ded84a4c36023c415da
Jagannath E Shahi <75128235+Jagannathes@users.noreply.github.com>
2022-02-24 00:44:00 +0530

    Update createConfigFiles.js

--- createConfigFiles.js
+++ createConfigFiles.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 
 const pathAdmin = './utils/db/serviceAccountKey.json'
 
-const adminPrivateKey = JSON.parse(process.env.private_key); 
+
 
 const adminConfig = `
 {
     "type":"${process.env.type}",
     "project_id":"${process.env.project_id}",
     "private_key_id":"${process.env.private_key_id}",
-    "private_key":"${adminPrivateKey}",
+    "private_key":"${process.env.private_key}",
     "client_email":"${process.env.client_email}",
     "client_id":"${process.env.client_id}",
     "auth_uri":"${process.env.auth_uri}",
@@ -21,7 +21,7 @@ const adminConfig = `
     
 fs.open(pathAdmin,'r',function(err, fd){
     if (err) {
-      fs.writeFileSync(pathAdmin, JSON.stringify(adminConfig), function(err) {
+      fs.writeFileSync(pathAdmin, adminConfig, function(err) {
           if(err) {
               console.log(err);
           }
