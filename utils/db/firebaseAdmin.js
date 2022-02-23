59a8a39bcd31fdf9e0ef7056674d5a19298c859d
Jagannath E Shahi <75128235+Jagannathes@users.noreply.github.com>
2022-02-24 00:27:33 +0530

    Update firebaseAdmin.js

--- utils/db/firebaseAdmin.js
+++ utils/db/firebaseAdmin.js
@@ -5,7 +5,7 @@ const serviceAccount = require('./serviceAccountKey.json');
 if (!admin.apps.length) {
     try {
       admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert(JSON.parse(serviceAccount)),
       });
     } catch (error) {
       console.log('Firebase admin initialization error', error.stack);
